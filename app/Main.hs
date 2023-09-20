7b8b75af8a46322a60b17a1d49da5d1ec3fca229
CamielYang <yangcamiel@gmail.com>
2023-09-20 16:42:48 +0200

    Add rahdom package + new particle Acid

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Data.ByteString (pack)
 import Data.Word
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import System.Random
 import Prelude hiding (Left, Right)
 
 width,
@@ -41,6 +42,7 @@ data ParticleType
   | Stone
   | Wood
   | Grass
+  | Acid
   | Empty
   deriving (Eq, Show, Enum, Bounded)
 
@@ -48,48 +50,30 @@ allParticles :: [ParticleType]
 allParticles = [(minBound :: ParticleType) ..]
 
 data ParticleData = Particle
-  { pVelocity :: Int,
-    pColor :: [Word8],
-    pDirections :: [Direction]
+  { pFallRate :: Int,
+    pColor :: [Word8]
   }
 
+defaultParticle =
+  Particle
+    { pFallRate = 1,
+      pColor = [255, 255, 255, 255]
+    }
+
+defaultStaticParticle =
+  Particle
+    { pFallRate = 0,
+      pColor = [255, 255, 255, 255]
+    }
+
 pd :: ParticleType -> ParticleData
-pd Sand =
-  Particle
-    { pVelocity = 10,
-      pColor = [218, 211, 165, 255],
-      pDirections = [Bottom, BottomLeft, BottomRight]
-    }
-pd Stone =
-  Particle
-    { pVelocity = 10,
-      pColor = [185, 185, 186, 255],
-      pDirections = []
-    }
-pd Wood =
-  Particle
-    { pVelocity = 10,
-      pColor = [136, 118, 71, 255],
-      pDirections = []
-    }
-pd Grass =
-  Particle
-    { pVelocity = 10,
-      pColor = [101, 159, 72, 255],
-      pDirections = []
-    }
-pd Water =
-  Particle
-    { pVelocity = 10,
-      pColor = [5, 138, 189, 255],
-      pDirections = [Bottom, BottomLeft, BottomRight, Left, Right]
-    }
-pd _ =
-  Particle
-    { pVelocity = 0,
-      pColor = [255, 255, 255, 255],
-      pDirections = []
-    }
+pd Sand = defaultParticle {pColor = [218, 211, 165, 255]}
+pd Water = defaultParticle {pColor = [5, 138, 189, 255]}
+pd Acid = defaultParticle {pColor = [114, 209, 68, 255]}
+pd Stone = defaultStaticParticle {pColor = [185, 185, 186, 255]}
+pd Wood = defaultStaticParticle {pColor = [136, 118, 71, 255]}
+pd Grass = defaultStaticParticle {pColor = [101, 159, 72, 255]}
+pd _ = defaultStaticParticle {pColor = [255, 255, 255, 255]}
 
 type Coord = (Int, Int)
 
@@ -179,6 +163,9 @@ getParticleFromCell (p, _) = p
 inBound :: Coord -> Bool
 inBound (x, y) = x <= sizeX' && x >= 0 && y <= sizeY' && y >= 0
 
+randInt :: Int -> Int -> IO Int
+randInt x y = getStdRandom (randomR (x, y))
+
 getParticle :: Grid -> Coord -> IO Cell
 getParticle g c
   | inBound c = readArray g c
@@ -214,57 +201,107 @@ createCell grid coord cell
 removeParticle :: Grid -> Coord -> Bool -> IO ()
 removeParticle g c u = createCell g c (Empty, u)
 
+isLiquid :: ParticleType -> Bool
+isLiquid p
+  | p == Water || p == Acid = True
+  | otherwise = False
+
 updateSand :: Coord -> Grid -> Bool -> IO ()
 updateSand c g u = do
   removeParticle g c u
-  (p1, _) <- getParticle g (getDir Bottom c)
+  (pB, _) <- getParticle g (getDir Bottom c)
   (p2, _) <- getParticle g (getDir BottomLeft c)
   (p3, _) <- getParticle g (getDir BottomRight c)
-  (p4, _) <- getParticle g (getDir Left c)
-  (p5, _) <- getParticle g (getDir Right c)
+  (pL, _) <- getParticle g (getDir Left c)
+  (pR, _) <- getParticle g (getDir Right c)
+  dirChange <- randInt 0 1
+  let pBL p = p2 == p && pL == p
+  let pBR p = p3 == p && pR == p
+
+  let plBL = isLiquid p2 && isLiquid pL
+  let plBR = isLiquid p3 && isLiquid pR
 
   let action
-        | p1 == Empty = createCell g (getDir Bottom c) (Sand, not u)
-        | p2 == Empty && p4 == Empty = createCell g (getDir BottomLeft c) (Sand, not u)
-        | p3 == Empty && p5 == Empty = createCell g (getDir BottomRight c) (Sand, not u)
-        | p1 == Water = do
+        | pB == Empty = createCell g (getDir Bottom c) (Sand, not u)
+        | pBL Empty && (not (pBR Empty) || dirChange == 0) = createCell g (getDir BottomLeft c) (Sand, not u)
+        | pBR Empty = createCell g (getDir BottomRight c) (Sand, not u)
+        | isLiquid pB = do
             createCell g (getDir Bottom c) (Sand, not u)
-            createCell g c (Water, not u)
-        | p2 == Water && p4 == Water = do
+            createCell g c (pB, not u)
+        | plBL && (not plBR || dirChange == 0) = createCell g (getDir BottomLeft c) (Sand, not u)
+        | plBL = do
             createCell g (getDir BottomLeft c) (Sand, not u)
-            createCell g c (Water, not u)
-        | p3 == Water && p5 == Water = do
+            createCell g c (p2, not u)
+        | plBR = do
             createCell g (getDir BottomRight c) (Sand, not u)
-            createCell g c (Water, not u)
+            createCell g c (p3, not u)
         | otherwise = createCell g c (Sand, not u)
   action
 
 updateWater :: Coord -> Grid -> Bool -> IO ()
 updateWater c g u = do
   removeParticle g c u
-  (p1, _) <- getParticle g (getDir Bottom c)
+  (pB, _) <- getParticle g (getDir Bottom c)
   (p2, _) <- getParticle g (getDir BottomLeft c)
   (p3, _) <- getParticle g (getDir BottomRight c)
-  (p4, _) <- getParticle g (getDir Left c)
-  (p5, _) <- getParticle g (getDir Right c)
+  (pL, _) <- getParticle g (getDir Left c)
+  (pR, _) <- getParticle g (getDir Right c)
+  dirChange <- randInt 0 1
+  let pBL = p2 == Empty && pL == Empty
+  let pBR = p3 == Empty && pR == Empty
 
   let action
-        | p1 == Empty = createCell g (getDir Bottom c) (Water, not u)
-        | p2 == Empty && p4 == Empty = createCell g (getDir BottomLeft c) (Water, not u)
-        | p3 == Empty && p5 == Empty = createCell g (getDir BottomRight c) (Water, not u)
-        | p4 == Empty = createCell g (getDir Left c) (Water, not u)
-        | p5 == Empty = createCell g (getDir Right c) (Water, not u)
+        | pB == Empty = createCell g (getDir Bottom c) (Water, not u)
+        | pBL && (not pBR || dirChange == 0) = createCell g (getDir BottomLeft c) (Water, not u)
+        | pBR = createCell g (getDir BottomRight c) (Water, not u)
+        | pL == Empty && (pR /= Empty || dirChange == 0) = createCell g (getDir Left c) (Water, not u)
+        | pR == Empty = createCell g (getDir Right c) (Water, not u)
         | otherwise = createCell g c (Water, not u)
+  action
+
+updateAcid :: Coord -> Grid -> Bool -> IO ()
+updateAcid c g u = do
+  removeParticle g c u
+  (pB, _) <- getParticle g (getDir Bottom c)
+  (p2, _) <- getParticle g (getDir BottomLeft c)
+  (p3, _) <- getParticle g (getDir BottomRight c)
+  (pL, _) <- getParticle g (getDir Left c)
+  (pR, _) <- getParticle g (getDir Right c)
+
+  dirChange <- randInt 0 1
+  stoneChange <- randInt 0 300
+  corrodeChange <- randInt 0 100
+
+  let pBL = p2 == Empty && pL == Empty
+  let pBR = p3 == Empty && pR == Empty
+
+  let corrose p v = p /= Empty && p /= Acid && ((p == Stone && stoneChange < v) || (p /= Stone && corrodeChange < v))
+
+  let corroseAction
+        | inBound (getDir Bottom c) && corrose pB 1 = createCell g (getDir Bottom c) (Empty, not u)
+        | inBound (getDir Left c) && corrose pL 2 = createCell g (getDir Left c) (Empty, not u)
+        | inBound (getDir Right c) && corrose pR 3 = createCell g (getDir Right c) (Empty, not u)
+        | otherwise = return ()
+
+  let action
+        | pB == Empty = createCell g (getDir Bottom c) (Acid, not u)
+        | pBL && (not pBR || dirChange == 0) = createCell g (getDir BottomLeft c) (Acid, not u)
+        | pBR = createCell g (getDir BottomRight c) (Acid, not u)
+        | pL == Empty && (pR /= Empty || dirChange == 0) = createCell g (getDir Left c) (Acid, not u)
+        | pR == Empty = createCell g (getDir Right c) (Acid, not u)
+        | otherwise = createCell g c (Acid, not u)
+  corroseAction
   action
 
 update :: Float -> GameState -> IO GameState
 update _ gameState@(GameState t g u md mp sp) = do
   forM_ [0 .. sizeY'] $ \y -> do
-    forM_ [0 .. sizeX'] $ \x -> do
+    forM_ (if u then [0 .. sizeX'] else reverse [0 .. sizeX']) $ \x -> do
       particle <- readArray g (x, y)
       let action
             | particle == (Sand, u) = updateSand (x, y) g u
             | particle == (Water, u) = updateWater (x, y) g u
+            | particle == (Acid, u) = updateAcid (x, y) g u
             | otherwise = return ()
       action
 
